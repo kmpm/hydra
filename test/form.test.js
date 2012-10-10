cf4301cd36d3e471501629d286a0a724863f51b1
Peter Magnusson <peter.magnusson@mustad.se>
2012-10-10 21:49:02 +0200

    redesigned forms some

--- test/form.test.js
+++ test/form.test.js
@@ -1,5 +1,6 @@
 var assert = require('assert')
   , should = require('should');
+
 var forms = require('../lib/forms');
 
 
@@ -8,46 +9,54 @@ describe("Form", function(){
 
   it("render widget", function(){
     var f = new forms.Form({'test':'data'});
-    for(var i in f.fields){
-      var field = f.fields[i];
-      field.should.have.property('render');
-      field.should.have.property('widget');
-      var widget = field.widget;
+    f.should.have.property('fields');
+    f.fields.should.have.length(1);
 
-      widget.should.have.property('render');
-      widget.should.have.property('settings');
+  
+    // for(var i in f.fields){
+    var field = f.fields[0];
+    field.should.have.property('render');
+    field.should.have.property('widget');
+    var widget = field.widget;
 
-      var result = field.render();
-      result.should.equal('<input type="text" name="test" id="id_test" value="data">');
-    }
+    widget.should.have.property('render');
+    widget.should.have.property('settings');
+
+    var result = field.render();
+    result.should.equal('<input type="text" name="test" id="id_test" value="data">');
+    // }
   });
 
   it("render TextAreaWidget", function(){
     var f = new forms.Form({'test':'data'}, {widgets:
         {test:new forms.TextAreaWidget({"class":"span12"})}
       });
-    for(var i in f.fields){
-      var field = f.fields[i];
-      field.should.have.property('render');
-      field.should.have.property('widget');
-      var widget = field.widget;
-
-      widget.should.have.property('render');
-      widget.should.have.property('settings');
-
-      var result = field.render();
-      result.should.equal('<textarea rows="5" cols="25" class="span12" name="test" id="id_test">data</textarea>');
-    }
+    f.should.have.property('fields');
+    f.fields.should.have.length(1);
+    var result = f.fields[0].render();
+    result.should.equal('<textarea rows="5" cols="25" class="span12" name="test" id="id_test">data</textarea>');
+    
   });
 
   it("render with order", function(){
     var f = new forms.Form({'a':'dataA', 'b':'dataB'}, 
       {order:['b']});
+    f.should.have.property('fields');
     var fields = f.fields;
     fields.should.have.length(2);
     fields[0].name.should.equal('b');
     fields[1].name.should.equal('a');
-    
+  });
+
+  it("hide and show", function(){
+    var f = new forms.Form({'a':'dataA', 'b':'dataB', c:'dataC'}, 
+      {order:['b'], hidden:['c']});
+    f.should.have.property('visible');
+    f.should.have.property('hidden');
+    f.visible.should.have.length(2);
+    f.hidden.should.have.length(1);
+    f.visible[0].name.should.equal('b');
+    f.hidden[0].name.should.equal('c');
   });
 
 });
