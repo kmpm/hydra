1929fcf8d7b3bdc96ee4a403700812ed2ba86bf9
Peter Magnusson <peter.magnusson@mustad.se>
2012-10-12 09:30:31 +0200

    using nconf for configuration

--- test/common.test.js
+++ test/common.test.js
@@ -7,7 +7,15 @@ var c = require('../lib/common');
 describe("common", function(){
   it("should have config", function(){
     c.should.have.property('config');
+
     var config = c.config;
-    config.should.have.property('testing', true);
+    config.should.have.property('get');
+    config.get('testing').should.equal(true);
+  });
+
+  it("should have log", function(){
+    c.should.have.property('log');
+    
+
   });
 });
